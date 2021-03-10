db8e8e510b18d420539618c11e9655bb1e8cb007
Sorab <sorab.khanna882@gmail.com>
2021-03-10 23:45:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    userInput: "",
+    fruitsArray: [],
+  };
+
+  clickHandler = (e) => {
+    e.preventDefault();
+    const splitting = this.state.userInput.split("-");
+    this.setState((prevState) => ({
+      userInput: "",
+      fruitsArray: [
+        ...prevState.fruitsArray,
+        { fruit: splitting[0], quantity: splitting[1] },
+      ],
+    }));
+  };
+
+  inputChangeHandler = (e) => {
+    this.setState({ userInput: e.target.value });
+  };
+
+  deleteFruit(fruit) {
+    return () => {
+      this.setState((prevState) => ({
+        fruitsArray: prevState.fruitsArray.filter(
+          (fruitItem) => fruitItem.fruit !== fruit
+        ),
+      }));
+    };
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <form onSubmit={this.clickHandler}>
+          <input
+            type="text"
+            id="name"
+            onChange={this.inputChangeHandler}
+            value={this.state.userInput}
+          />
+          <br />
+          <button>Submit</button>
+        </form>
+        <table>
+          <tbody>
+            {this.state.fruitsArray.map((fruit) => {
+              return (
+                <tr key={fruit.fruit}>
+                  <td>{fruit.fruit}</td>
+                  <td>{fruit.quantity}</td>
+                  <td>
+                    <button onClick={this.deleteFruit(fruit.fruit)}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
-
-export default App;
